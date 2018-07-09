70723965d2a858600dea4b348dccb112e71da097
hg <hg@wtf.xyz>
2018-07-09 06:27:48 -0400

    lot o tings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -22,14 +24,16 @@ const appRoutes: Routes = [
     ProjectsComponent,
     ContactComponent,
     HomeComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing:  true } // for debugging
-    )
+      { enableTracing:  true } // debugging
+    ),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'k-contact',
@@ -11,15 +12,31 @@ export class ContactComponent implements OnInit {
   private contactApiUrl: string = 'https://a605xjaczg.execute-api.us-east-1.amazonaws.com/pp/contact';
   private name: string;
   private message: string;
+  private messageConfirmation: string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
 // maybe maybe service later    
-    this.http.post(this.contactApiUrl, { name: this.name, message: this.message });
+    this.http.post(this.contactApiUrl, { name: this.name, message: this.message })
+      .subscribe(
+        data  => { this.messageSuccess(); },
+        error => { this.messageFailure(error); }
+      );
+  }
+
+  messageSuccess() {
+    this.name = '';
+    this.message = '';
+    this.messageConfirmation = "Thanks for the message :)";  
+  }
+
+  messageFailure(error: any) {
+    console.log(error);
+    this.messageConfirmation = "Uh oh... Try again later :("
   }
 
 }
