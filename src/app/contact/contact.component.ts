50c115423417a155e1cf0ff32da20b1ec2276347
hg <hg@wtf.xyz>
2018-07-10 12:50:12 -0400

    contact form better

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
@@ -13,6 +14,8 @@ export class ContactComponent implements OnInit {
   private name: string;
   private message: string;
   private messageConfirmation: string;
+  private sending: boolean = false;
+  @ViewChild(NgForm) contactForm: NgForm;
 
   constructor(private http: HttpClient, private router: Router) { }
 
@@ -21,6 +24,7 @@ export class ContactComponent implements OnInit {
 
   onSubmit() {
 // maybe maybe service later    
+    this.sending = true;
     this.http.post(this.contactApiUrl, { name: this.name, message: this.message })
       .subscribe(
         data  => { this.messageSuccess(); },
@@ -29,14 +33,15 @@ export class ContactComponent implements OnInit {
   }
 
   messageSuccess() {
-    this.name = '';
-    this.message = '';
+    this.contactForm.reset();
     this.messageConfirmation = "Thanks for the message :)";  
+    this.sending = false;
   }
 
   messageFailure(error: any) {
     console.log(error);
     this.messageConfirmation = "Uh oh... Try again later :("
+    this.sending = false;
   }
 
 }
